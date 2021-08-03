66f074aa717062e22f24a7287582cf8ab878ef6f
Christian Toves <christiano.toves@gmail.com>
2021-08-03 16:32:40 -0700

    draft 2

--- Basics.java
+++ Basics.java
@@ -2,68 +2,61 @@ import java.util.ArrayList;
 
 public class Basics {
     public void printNums(){
-        for(int i=0; i<256; i++){
+        for (int i=0; i<256; i++){
             System.out.println(i);
         }
     }
-    
-    
+
     public void printOddNums(){
-        for(int i=0; i<256; i++){
-            if(i%2!=0){
+        for (int i = 0; i < 256; i++){
+            if (i%2 != 0){
                 System.out.println(i);
             }
         }
     }
-    
-    
+
     public void sumTo255(){
-        int sum=0;
-        for(int i=0; i<256; i++){
-            sum+=i;
+        int sum = 0;
+        for (int i = 0; i < 256; i++){
+            sum += i;
         }
         System.out.println(sum);
     }
 
-    
-    public void iteratingThroughAnArray(int[] arr){
-        for(int i=0; i<arr.length; i++){
+    public void iteratingThruAnArray(int[] arr){
+        for (int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
-        }
+        } 
     }
-    
 
     public void findMax(int[] arr){
-        int max=arr[0];
-        for(int i=1; i<arr.length; i++){
-            if(arr[i]>max){
-                max=arr[i];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] > max){
+                max = arr[i];
             }
         }
         System.out.println(max);
     }
 
-
-    public void arrayWithOddNumbers() {
+    public void arrayWithOddNums(){
         ArrayList<Integer> results = new ArrayList<Integer>();
-        for(int i=1; i<256; i=i+2){
+        for (int i = 1; i < 256; i = i+2){
             results.add(i);
         }
         System.out.println(results);
     }
 
-    //Ninja Bonus
-    public void getAverage(int[] arr) {
+    public void getAverage(int[] arr){
         double avg = 0;
-        for(int i = 0; i < arr.length; i++){
+        for (int i=0; i<arr.length; i++){
             avg += (double)i;
         }
         avg = avg / (double) arr.length;
         System.out.println(avg);
     }
 
-
-    public void greaterThanY(int[] arr, int val) {
+    public void greaterThanY(int[] arr, int val){
         ArrayList<Integer> results = new ArrayList<Integer>();
         for(int i=0; i<arr.length; i++){
             if(arr[i]>val){
@@ -73,7 +66,6 @@ public class Basics {
         System.out.println(results.size());
     }
 
-
     public void squareTheValues(int[] arr){
         ArrayList<Integer> results = new ArrayList<Integer>();
         for(int i=0; i<arr.length; i++){
@@ -82,9 +74,8 @@ public class Basics {
         System.out.println(results);
     }
 
-
-    public int[] eliminateNegativeNumbers(int[] arr){
-        for(int i = 0; i < arr.length; i++){
+    public int[] eliminateNegativeNums(int[] arr){
+        for (int i = 0; i < arr.length; i++){
             if(arr[i]<0){
                 arr[i]=0;
             }
@@ -92,12 +83,11 @@ public class Basics {
         return arr;
     }
 
-    //Sensei Bonus
     public double[] maxMinAvg(int[] arr){
         int max = arr[0];
         int min = arr[0];
         double avg = (double)arr[0];
-        for(int i=1; i<arr.length; i++){
+        for (int i=1; i<arr.length; i++){
             if(arr[i]>max){
                 max=arr[i];
             }
@@ -106,15 +96,13 @@ public class Basics {
             }
             avg+=(double)arr[i];
         }
-        avg=avg/(double)arr.length;
+        avg = avg / (double) arr.length;
         double[] results = {(double)max, (double)min, avg};
         return results;
     }
-
-
     public int[] shifter(int[] arr){
-        for(int i=1; i<arr.length; i++){
-            arr[i-1]=arr[i];
+        for (int i=1; i<arr.length; i++){
+            arr[i-1]= arr[i];
         }
         arr[arr.length-1]=0;
         return arr;
--- TestBasics.java
+++ TestBasics.java
@@ -7,20 +7,20 @@ public class TestBasics {
         test.printOddNums();
         test.sumTo255();
         int[] numArr = {5,4,3,2,1};
-        test.iteratingThroughAnArray(numArr);
+        test.iteratingThruAnArray(numArr);
         test.findMax(numArr);
-        test.arrayWithOddNumbers();
+        test.arrayWithOddNums();
         test.getAverage(numArr);
-        test.greaterThanY(numArr, 3);
+        test.greaterThanY(numArr,3);
         int[] testNums = {1, 5, 10, -2};
         test.squareTheValues(testNums);
-        int[] results = test.eliminateNegativeNumbers(testNums);
+        int[] results = test.eliminateNegativeNums(testNums);
         System.out.println(Arrays.toString(results));
 
-        double[] results2 = test.maxMinAvg(testNums);
+        double[] results2= test.maxMinAvg(testNums);
         System.out.println(Arrays.toString(results2));
 
         int[] results3 = test.shifter(testNums);
-        System.out.println(Arrays.toString(results3));
+        System.out.println(Arrays.toString(results));
     }
 }
